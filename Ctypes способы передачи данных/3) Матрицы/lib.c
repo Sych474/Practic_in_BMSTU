7130000c2daf300d32d54e9f0e42192a7b8a2f9f
Sych474 <rk474@yandex.ru>
2016-07-06 03:08:51 +0300

    Release #1
    First Release version

--- Ctypes способы передачи данных/3) Матрицы/lib.c
+++ Ctypes способы передачи данных/3) Матрицы/lib.c
@@ -8,6 +8,7 @@ void get_matr(const int N, int matr[][N],int ans[N])
             ans[i] = matr[i][j];
     for (i = 0; i < N; i++)
         for (j = 0; j < N; j++)
-            matr[i][j] = i*j;
-       
+            matr[i][j] = i*j;      
 }
+
+
